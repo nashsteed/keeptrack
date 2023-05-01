516910d36f1915547207a40afd5e722faba4d553
Connor Wilson <Ronnoc0130@gmail.com>
2023-04-30 21:43:17 -0400

    Removed some commented out code.

--- header.php
+++ header.php
@@ -17,30 +17,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         <meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
     </head>
     <body>
-        <header>  
-            <!-- <nav class="navbar navbar-light" style="background-color: #32594a;">
-                <div class="container-fluid">            
-                    <a class="navbar-brand" href="home.php" style="font-size: 26px;" style="font-family: Brush Script MT;">
-                        <img src="keepTracklogo.png" width = 200>
-                    </a>
-                    <button class="btn btn-light" style="float: left" disabled>Logged in as: <?php echo $_SESSION['username']; ?></button>
-                    <div class='btn-toolbar pull-right' class ="navbar-nav ml-auto">
-                        <button class="btn btn-light" style="float: right" href=home.php></button>
-                        <button class="btn btn-light" style="float: right" href=viewDB.php></button>
-
-                        <form action="viewDB.php" method="post">
-                            <input type="submit" name="viewdbbtn" class="btn btn-light" value="View Database" style="float: right"></input>
-                        </form>
-                        <form action="home.php" method="post">
-                            <input type="submit" name="homebtn" class="btn btn-light" value="Home" style="float: right"></input>
-                        </form>
-
-                        <form action="header.php" method="post">
-                            <input type="submit" name="logoutbtn" class="btn btn-light" value="Log Out" style="float: right"></input>
-                        </form>
-                    </div>
-                </div>
-            </nav> -->
+        <header> 
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="home.php" style="font-size: 26px;" style="font-family: Brush Script MT;">
